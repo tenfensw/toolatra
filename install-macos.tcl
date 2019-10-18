aa466d84431fb02b38ea5246ceeb0861d8abe104
Tim K <timprogrammer@rambler.ru>
2019-10-18 15:09:45 +0300

    Added install-macos.tcl

---
+++ install-macos.tcl
@@ -0,0 +1,14 @@
+#!/usr/bin/env tclsh
+set directory "$::env(HOME)/Library/Tcl/toolatra-master"
+if {[file isdirectory $directory]} {
+	file delete -force $directory
+}
+
+file mkdir $directory
+foreach fn {toolatra_http.tcl toolatra_templates.tcl pkgIndex.tcl} {
+	file copy $fn "$directory/$fn"
+}
+
+puts done
+exit 0
+
